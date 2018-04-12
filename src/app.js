cc90e2d6aa2072c4232e22b99eb11c66d1809aff
mh4722 <hassan.motivala@gmail.com>
2018-04-12 14:13:50 -0400

    Add a route to check if application runs properly on the server.

--- src/app.js
+++ src/app.js
@@ -34,4 +34,9 @@ const publicPath = path.resolve(__dirname, "public");
 app.use(express.static(publicPath));
 
 
+app.get('/', (req, res) => {
+	res.send("HI. Response recieved");
+});
+
+
 app.listen(process.env.PORT || 3000);
