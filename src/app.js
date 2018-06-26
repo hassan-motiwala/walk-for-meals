240370d04a764f30174b7a873fa33c6427561cb7
mh4722 <hassan.motivala@gmail.com>
2018-06-26 01:01:59 -0400

    Debugging for Heroku

--- src/app.js
+++ src/app.js
@@ -298,4 +298,4 @@ app.post('/restaurants/pickLocation', (req, res) => {
 /********************************************************************/
 
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 5000);
